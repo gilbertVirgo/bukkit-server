0ace88576235934cf0ef5af4d8ee246690c78036
Gil <gilbertjvirgo@gmail.com>
2021-12-23 20:05:45 +0000

    Update jar obsolete

--- update-jar.js
+++ update-jar.js
@@ -1,14 +1,17 @@
 const fetch = require("node-fetch");
-const {exec} = require("child_process");
+const { exec } = require("child_process");
+const fs = require("fs").promises;
 
-(async function() {
-	const res = await fetch("https://papermc.io/api/v1/paper");
-	const {versions} = await res.json();
+(async function () {
+	console.log("Update PaperMC script offline.");
+	console.log(
+		"They've updated their site and rendered my scraper code obsolete."
+	);
 
-	const download = `https://papermc.io/api/v1/paper/${versions[0]}/latest/download`;
+	const downloadLink = "Fetch from site...";
 
-	const { stdout, stderr } = await exec(`curl ${download} -o bukkit.jar`);
+	// const { stdout, stderr } = await exec(`curl ${downloadLink} -o bukkit.jar`);
 
-	stdout.on("data", console.log);
-	stderr.on("data", console.log);
+	// stdout.on("data", console.log);
+	// stderr.on("data", console.log);
 })();
